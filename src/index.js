a8b270dfb0af49df57f5a0ea9b1adcc32f5de36b
oshanedesign <oshane.design@gmail.com>
2022-04-25 16:49:15 -0500

    added src folder

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+//component files
+import TodoContainer from "./components/TodoContainer"
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
+
+const element = <h1>Hello from Create React App</h1>
+
+ReactDOM.render(element, document.getElementById("root"))
