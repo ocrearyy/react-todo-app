8c804a205e9a7111c0dae2df4e91ca9590428cc2
oshanedesign <oshane.design@gmail.com>
2022-04-26 16:22:23 -0500

    merge main

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer"
 
 ReactDOM.render(
-<React.StrictMode>
-  <TodoContainer />
-</React.StrictMode>, 
-document.getElementById("root"))
-
-const element = <h1>hi from Create React App</h1>
-
-ReactDOM.render(element, document.getElementById("root"))
+    <React.StrictMode>
+      <TodoContainer />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+  
